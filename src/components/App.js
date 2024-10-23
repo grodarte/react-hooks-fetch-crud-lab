1b1bcea2c2b7ac328a353c4bf10577e9446e0571
Gabrielle <ms.grodarte@gmail.com>
2024-10-23 15:01:20 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+  useEffect(()=>{
+    fetch(`http://localhost:4000/questions`)
+    .then(r=>r.json())
+    .then(questionData=>setQuestions(questionData))
+  }, [])
+
+  function handleChangeAnswer(changedQuestion){
+    const updatedQuestions = questions.map(question=>{
+      if(question.id === changedQuestion.id){
+        return changedQuestion
+      } else {
+        return question
+      }
+    })
+    setQuestions(updatedQuestions)
+  }
+
+  function handleDeleteQuestion(deletedQuestion){
+    const updatedQuestions = questions.filter(question=>question.id !== deletedQuestion.id)
+    setQuestions(updatedQuestions)
+  }
+
+  function handleAddNewQuestion(newQuestion){
+    setQuestions([
+      ...questions,
+      newQuestion
+    ])
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddNewQuestion={handleAddNewQuestion}/> : <QuestionList questionData={questions} onDeleteQuestion={handleDeleteQuestion} onChangeAnswer={handleChangeAnswer}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,12 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questionData, onDeleteQuestion, onChangeAnswer }) {
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionData.map(question=><QuestionItem key={question.id} question={question} onDeleteQuestion={onDeleteQuestion} onChangeAnswer={onChangeAnswer}/>)}</ul>
     </section>
   );
 }
